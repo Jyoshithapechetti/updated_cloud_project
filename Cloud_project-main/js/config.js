6100b37432073634c55033f9c2822b0cd8e7b286
Jyoshitha Pechetti <jyoshithapechetti03@gmail.com>
2025-05-14 16:57:50 +0530

    Add files via upload

---
+++ Cloud_project-main/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_yBo4MHnDY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '28s8dq63c6or49nf1vmg14ro9f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://psh6qaosmg.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
